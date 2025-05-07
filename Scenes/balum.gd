a04f1f43bd6a1aa9942e27d6fee8f76bf9852bb0
Sousa <trsh9sousa@gmail.com>
2025-05-07 14:33:09 -0300

    Cria base do jogo

---
+++ Scenes/balum.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED = 200
+
+func _physics_process(delta):
+	var input_vector = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	).normalized()
+
+	velocity = input_vector * SPEED
+	move_and_slide()
